efb735184ffffbffc393fbeff5231eb01d91f73d
Akshat Jain <realakshatjain@gmail.com>
2025-07-31 17:33:23 +0530

    frontend setup done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-9xl">
+        Kaam karo saare Frontend kal tak pura chaiye
+      </h1>
+    </>
+  );
+}
+
+export default App;
